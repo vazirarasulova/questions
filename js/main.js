a47f05c7ae28be7b1a41cd59bf015f5f19c6f6aa
vazirarasulova <rasulovavazira202gmail.com>
2022-06-05 23:57:28 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var username = prompt("Ismingizni kiriting");
+
+var firstQuestion = prompt("Alertning danger holatini yaratish uchun classlar ketma ketligini yozing"); // alert alert-danger
+var secondQuestion = prompt("Bootstrapda imagelarni qaysi class orqali responsive qilish mumkin?");  //.img-fluid. 
+var thirdQuestion = prompt("Tablening qatorlarini zebra ko'rinishga olib kelish uchun bootstrap classini yozing"); //.table-stripet
+var fourthQuestion = prompt("Bootstrapda nechi xil breakpoints turlari bor?"); // 6 xil
+var fifthQuestion = prompt("Containerning nechi xil turlari mavjud?"); // 2 xil
+var sixthQuestion = prompt("Badge classi htmlning qaysi (tag)-ida yoziladi"); // span
+
+console.log(username + "\n" + firstQuestion + "\n" + secondQuestion + "\n" + thirdQuestion + "\n" + fourthQuestion + "\n" + fifthQuestion + "\n" + sixthQuestion);
